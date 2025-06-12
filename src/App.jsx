e292b0b11048aad1ca62f2b00cc8fb24eaefd4df
BettBernarda <megan.silva182006@gmail.com>
2025-06-12 13:31:40 -0300

    Tranferi o componente CustomTabPanel para um jsx individual

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,12 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 import Tabelazona from './components/Tabelazona';
+import CustomTabPanel from './components/tab';
+import { a11yProps } from './components/tab';
 
-function CustomTabPanel(props) {
-  const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-    </div>
-  );
-}
 
-CustomTabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
 
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
 
 export default function App() {
   const [value, setValue] = React.useState(0);
@@ -54,9 +31,6 @@ export default function App() {
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
         <Tabelazona></Tabelazona>
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={2}>
-        Item Three
       </CustomTabPanel>
     </Box>
   );
