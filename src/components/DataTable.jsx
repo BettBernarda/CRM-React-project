045f46ef7eee92acde020dd64e9ce5a1b30db421
Bruno <brunombonifacio@gmail.com>
2025-06-13 14:06:55 -0300

    Alterado nome de Tabelazona para DataTable

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -19,17 +19,19 @@ const rows = [
   createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
 
-export default function Tabelazona() {
+export default function DataTable({ items, labels }) {
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            {labels.map((label, id) => {
+              if (id == 0) {
+                return <TableCell>{label}</TableCell>
+              }
+
+              return <TableCell align="right">{label}</TableCell>
+              })}
           </TableRow>
         </TableHead>
         <TableBody>
