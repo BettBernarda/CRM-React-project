4ca90f4dd3e842ddf358898387e8ad6c49668396
Bruno <brunombonifacio@gmail.com>
2025-06-13 14:02:14 -0300

    Alterado App para apenas retornar o RouterProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,22 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
-import Tabelazona from './components/Tabelazona';
-import CustomTabPanel from './components/tab';
-import { a11yProps } from './components/tab';
-
-
-
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import DefaultTemplate from "./pages/DefaultTemplate";
+import ProductsPage from "./pages/ProductsPage";
 
 export default function App() {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <DefaultTemplate />,
+      children: [
+        {
+          path: "/produtos",
+          element: <ProductsPage />
+        }
+      ]
+    }
+  ])
 
   return (
-      <Box sx={{ width: '100%' }}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-            <Tab label="Vendidos" {...a11yProps(0)} />
-            <Tab label="Em estoque" {...a11yProps(1)} />
-          </Tabs>
-        </Box>
-        <CustomTabPanel value={value} index={0}>
-          <Tabelazona></Tabelazona>
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={1}>
-          <Tabelazona></Tabelazona>
-        </CustomTabPanel>
-      </Box>
+    <RouterProvider router={router}/>
   );
 }
