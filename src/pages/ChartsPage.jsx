60e1606f3e51beae4aecb5328042e3977d007755
BettBernarda <megan.silva182006@gmail.com>
2025-06-18 19:58:54 -0300

    Componentizei a função de grafico de linha

--- src/pages/ChartsPage.jsx
+++ src/pages/ChartsPage.jsx
@@ -1,16 +1,9 @@
 import * as React from 'react';
-import { LineChart } from '@mui/x-charts/LineChart';
 
-export default function ChartsPage() {
+import ChartLine from '../components/Chats';
+
+export default function Chartspage() {
   return (
-    <LineChart
-      xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-      series={[
-        {
-          data: [2, 5.5, 2, 8.5, 1.5, 5],
-        },
-      ]}
-      height={300}
-    />
+    <ChartLine/>
   );
 }
