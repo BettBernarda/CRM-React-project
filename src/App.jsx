e43a45ee11e574116fb049fda0f64610d674baaf
BettBernarda <megan.silva182006@gmail.com>
2025-06-18 19:49:05 -0300

    Adicionei o componente de gráfico de linha do MUI e aloquei no menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import DefaultTemplate from "./pages/DefaultTemplate";
 import ProductsPage from "./pages/ProductsPage";
+import ChartsPage from "./pages/ChartsPage"
 
 export default function App() {
   const router = createBrowserRouter([
@@ -11,7 +12,12 @@ export default function App() {
         {
           path: "/produtos",
           element: <ProductsPage />
+        },
+        {
+          path: "/",
+          element: <ChartsPage/>
         }
+
       ]
     }
   ])
