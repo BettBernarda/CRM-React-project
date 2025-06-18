583f73bedf618ccad69beb46089c7b26585c7025
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2025-06-18 19:33:54 -0300

    Alterado BasicBreadcrumbs para PathBreadcrumbs e movida lógica dos links para dentro dele

--- src/pages/DefaultTemplate.jsx
+++ src/pages/DefaultTemplate.jsx
@@ -1,7 +1,7 @@
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import ResponsiveAppBar from "../components/ResponsiveAppBar";
-import BasicBreadcrumbs from "../components/BasicBreadcrumbs";
+import PathBreadcrumbs from "../components/PathBreadcrumbs";
 
 export default function DefaultTemplate() {
     const theme = createTheme({
@@ -10,37 +10,14 @@ export default function DefaultTemplate() {
       }
     });
 
-    const location = useLocation()
-
-    // TODO: MELHORAR ESSE CODIGO E O DE BasicBreadcrumbs PELO AMOR DE 
-    const generateBreadcrumbs = () => {
-      const splitPath = location.pathname.split('/').filter(e => e).map(e => e[0].toUpperCase() + e.substring(1))
-      
-      const pathsByTitle = new Map()
-      pathsByTitle.set('Home', {
-        path: '/',
-        selected: location.pathname == '/'
-      })
-      
-      splitPath.forEach((pathTitle, i) => {
-        const previousPath = pathsByTitle.get(splitPath[i - 1])?.path ?? ''
-        const fullPath = previousPath + '/' + pathTitle.toLowerCase()
-        
-        pathsByTitle.set(pathTitle, {
-          path: fullPath,
-          selected: location.pathname == fullPath
-        })
-      })
-      
-      return <BasicBreadcrumbs links={pathsByTitle}></BasicBreadcrumbs>
-    }
-
     return (
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <ResponsiveAppBar></ResponsiveAppBar>
-        {generateBreadcrumbs()}
-        <Outlet></Outlet>
+        <div className="m-4">
+          <PathBreadcrumbs></PathBreadcrumbs>
+          <Outlet></Outlet>
+        </div>
       </ThemeProvider>
     )
 }
