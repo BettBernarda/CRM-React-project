8a211c167275eeab7d6f3df12b008578386035c8
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2025-06-18 20:28:54 -0300

    Definido baseURL do axios para não precisar escrever os endpoints inteiros, alterado ProductsPage para exibir os nomes das categorias dos produtos e dos fornecedores, ao inveś de apenas seus IDs

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import DefaultTemplate from "./pages/DefaultTemplate";
 import ProductsPage from "./pages/ProductsPage";
 import ChartsPage from "./pages/ChartsPage"
+import axios from "axios";
 
 export default function App() {
+  axios.defaults.baseURL = 'http://localhost:3000'
+  
   const router = createBrowserRouter([
     {
       path: "/",
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,90 +1,105 @@
-import { Box, Paper, Tab, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tabs } from "@mui/material";
+import { Box, Fab, Paper, Tab, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tabs } from "@mui/material";
 import { useEffect, useState } from "react";
 import CustomTabPanel, { a11yProps } from "../components/CustomTabPanel";
 import axios from "axios";
+import { Add as AddIcon } from "@mui/icons-material";
 
 export default function ItemsPage() {
   const [value, setValue] = useState(0);
   const [productsList, setProductsList] = useState([])
+  const [fornecedoresList, setFornecedoresList] = useState([])
+  const [categoriasList, setCategoriasList] = useState([])
 
   useEffect(() => {
-    axios.get(`http://localhost:3000/produtos`).then(response => setProductsList(response.data))
+    axios.get(`/produtos`).then(response => setProductsList(response.data))
+    axios.get(`/fornecedores`).then(response => setFornecedoresList(response.data))
+    axios.get(`/categorias_produto`).then(response => setCategoriasList(response.data))
   }, [])
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
+  const findFornecedorNameById = id => fornecedoresList.find(fornecedor => fornecedor.id == id).nome
+  const findCategoriaNameById = id => categoriasList.find(categoria => categoria.id == id).nome
+
   return (
-      <Box sx={{ width: '100%' }}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-            <Tab label="Vendidos" {...a11yProps(0)} />
-            <Tab label="Em estoque" {...a11yProps(1)} />
-          </Tabs>
+      <>
+        <Box sx={{ width: '100%' }}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+              <Tab label="Vendidos" {...a11yProps(0)} />
+              <Tab label="Em estoque" {...a11yProps(1)} />
+            </Tabs>
+          </Box>
+          <CustomTabPanel value={value} index={0}>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>ID</TableCell>
+                    <TableCell align="right">Status</TableCell>
+                    <TableCell align="right">Descrição</TableCell>
+                    <TableCell align="right">Preço</TableCell>
+                    <TableCell align="right">Categoria</TableCell>
+                    <TableCell align="right">Fornecedor</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {productsList.map(product => (
+                    <TableRow
+                      key={product.id}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">{product.id}</TableCell>
+                      <TableCell align="right">{product.status ? 'Ativo' : 'Inativo'}</TableCell>
+                      <TableCell align="right">{product.descricao}</TableCell>
+                      <TableCell align="right">{product.preco}</TableCell>
+                      <TableCell align="right">{findCategoriaNameById(product.categoria_id)}</TableCell>
+                      <TableCell align="right">{findFornecedorNameById(product.fornecedor_id)}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <Box sx={{ '& > :not(style)': { m: 1 } }} className="flex justify-end">
+              <Fab color="primary" aria-label="add">
+                <AddIcon />
+              </Fab>
+            </Box>
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={1}>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>ID</TableCell>
+                    <TableCell align="right">Status</TableCell>
+                    <TableCell align="right">Descrição</TableCell>
+                    <TableCell align="right">Preço</TableCell>
+                    <TableCell align="right">Categoria</TableCell>
+                    <TableCell align="right">Fornecedor</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {productsList.map(product => (
+                    <TableRow
+                      key={product.id}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">{product.id}</TableCell>
+                      <TableCell align="right">{product.status ? 'Ativo' : 'Inativo'}</TableCell>
+                      <TableCell align="right">{product.descricao}</TableCell>
+                      <TableCell align="right">{product.preco}</TableCell>
+                      <TableCell align="right">{product.categoria_id}</TableCell>
+                      <TableCell align="right">{product.fornecedor_id}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </CustomTabPanel>
         </Box>
-        <CustomTabPanel value={value} index={0}>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>ID</TableCell>
-                  <TableCell align="right">Status</TableCell>
-                  <TableCell align="right">Descrição</TableCell>
-                  <TableCell align="right">Preço</TableCell>
-                  <TableCell align="right">Categoria</TableCell>
-                  <TableCell align="right">Fornecedor</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {productsList.map(product => (
-                  <TableRow
-                    key={product.id}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >{console.log(product)}
-                    <TableCell component="th" scope="row">{product.id.toString()}</TableCell>
-                    <TableCell align="right">{product.status.toString()}</TableCell>
-                    <TableCell align="right">{product.descricao.toString()}</TableCell>
-                    <TableCell align="right">{product.preco.toString()}</TableCell>
-                    <TableCell align="right">{product.categoria_id.toString()}</TableCell>
-                    <TableCell align="right">{product.fornecedor_id.toString()}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={1}>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>ID</TableCell>
-                  <TableCell align="right">Status</TableCell>
-                  <TableCell align="right">Descrição</TableCell>
-                  <TableCell align="right">Preço</TableCell>
-                  <TableCell align="right">Categoria</TableCell>
-                  <TableCell align="right">Fornecedor</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {productsList.map(product => (
-                  <TableRow
-                    key={product.id}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >{console.log(product)}
-                    <TableCell component="th" scope="row">{product.id.toString()}</TableCell>
-                    <TableCell align="right">{product.status.toString()}</TableCell>
-                    <TableCell align="right">{product.descricao.toString()}</TableCell>
-                    <TableCell align="right">{product.preco.toString()}</TableCell>
-                    <TableCell align="right">{product.categoria_id.toString()}</TableCell>
-                    <TableCell align="right">{product.fornecedor_id.toString()}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </CustomTabPanel>
-      </Box>
+      </>
   );
 }
