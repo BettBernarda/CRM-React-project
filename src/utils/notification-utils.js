49e02d6325955f4537e2ea55a5bd2d3bece89536
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2025-06-19 17:40:41 -0300

    Criada função para mostrar mensagem de erro

--- src/utils/notification-utils.js
+++ src/utils/notification-utils.js
@@ -1,4 +1,5 @@
 import { enqueueSnackbar } from "notistack"
 
 export const showMessage = (message, variant) => enqueueSnackbar(message, { variant })
-export const showMessageSuccess = message => showMessage(message, 'success')
+export const showMessageSuccess = message => enqueueSnackbar(message, { variant: 'success' })
+export const showMessageError = message => enqueueSnackbar(message, { variant: 'error' })
