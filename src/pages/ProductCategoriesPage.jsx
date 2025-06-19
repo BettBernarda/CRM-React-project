537deeff589a2b3e7cfcac3c3e09bc923ffb41ba
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2025-06-19 19:00:07 -0300

    Adicionado filtro de busca na listagem de categorias de produtos

--- src/pages/ProductCategoriesPage.jsx
+++ src/pages/ProductCategoriesPage.jsx
@@ -1,11 +1,12 @@
 import { Add as AddIcon } from "@mui/icons-material"
-import { Box, Fab, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip, Typography } from "@mui/material"
+import { Box, Fab, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Tooltip, Typography } from "@mui/material"
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 export default function ProductCategoriesPage() {
   const [categoriasList, setCategoriasList] = useState([])
+  const [searchText, setSearchText] = useState('')
 
   const navigate = useNavigate()
   
@@ -18,6 +19,13 @@ export default function ProductCategoriesPage() {
       <Typography variant="h4" gutterBottom>
         Categorias de Produtos
       </Typography>
+      <TextField fullWidth
+          label="Pesquise por uma categoria"
+          type="search"
+          variant="standard"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
       <Box sx={{ width: '100%' }}>
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -29,19 +37,21 @@ export default function ProductCategoriesPage() {
               </TableRow>
             </TableHead>
             <TableBody>
-              {categoriasList.map(categoria => (
-                <TableRow
-                  key={categoria.id}
-                  sx={{ 
-                    '&:last-child td, &:last-child th': { border: 0 },
-                    '&:hover': { cursor: 'pointer' }
-                  }}
-                  onClick={() => navigate(`/produtos/categorias/${categoria.id}`)}
-                >
-                  <TableCell component="th" scope="row">{categoria.id}</TableCell>
-                  <TableCell align="right">{categoria.nome}</TableCell>
-                  <TableCell align="right">{categoria.descricao}</TableCell>
-                </TableRow>
+              {categoriasList
+                .filter(categoria => categoria.nome.toUpperCase().includes(searchText.toUpperCase()))
+                .map(categoria => (
+                  <TableRow
+                    key={categoria.id}
+                    sx={{ 
+                      '&:last-child td, &:last-child th': { border: 0 },
+                      '&:hover': { cursor: 'pointer' }
+                    }}
+                    onClick={() => navigate(`/produtos/categorias/${categoria.id}`)}
+                  >
+                    <TableCell component="th" scope="row">{categoria.id}</TableCell>
+                    <TableCell align="right">{categoria.nome}</TableCell>
+                    <TableCell align="right">{categoria.descricao}</TableCell>
+                  </TableRow>
               ))}
             </TableBody>
           </Table>
