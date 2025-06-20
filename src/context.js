d0fd6aa8e47087c0626960bd704eb7f615cb1328
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2025-06-19 21:39:56 -0300

    Criada função de logout em context.js

--- src/context.js
+++ src/context.js
@@ -1,3 +1,12 @@
 import { createContext } from "react";
 
 export const UserContext = createContext()
+
+export const logout = (userContext, navigate) => {
+  userContext.id = null
+  localStorage.removeItem('userId')
+
+  if (navigate) {
+    navigate('/login')
+  }
+}
