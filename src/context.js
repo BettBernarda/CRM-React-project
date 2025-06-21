052af8ea727cf16adee30a3379a47506513570a2
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2025-06-21 12:29:35 -0300

    Movida função de login para context.js

--- src/context.js
+++ src/context.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { createContext } from "react";
+import { showMessageError, showMessageSuccess } from "./utils/notification-utils";
 
 export const UserContext = createContext()
 
@@ -9,4 +11,20 @@ export const logout = (userContext, navigate) => {
   if (navigate) {
     navigate('/login')
   }
+}
+
+export const login = async (user, userContext, navigate, pageToRedirect = '') => {
+  const result = await axios.get(`/usuarios?email=${user.email}&senha=${user.senha}&ativo=true`)
+  const actualUser = result.data[0]
+
+  if (!actualUser) {
+    showMessageError('Não existe usuário com este cadastro')
+    return
+  }
+
+  userContext.id = actualUser.id
+  sessionStorage.setItem('userId', actualUser.id)
+  showMessageSuccess('Login realizado com sucesso!')
+
+  navigate(`/${pageToRedirect}`)
 }
